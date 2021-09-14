0c4b36d6d235e3fb704b41e951d7985c47585b08
sushi1129 <88273491+sushi1129@users.noreply.github.com>
2021-09-14 21:32:40 +0900

    バリデーション設定

--- app/models/knowledge.rb
+++ app/models/knowledge.rb
@@ -1,4 +1,8 @@
 class Knowledge < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  with_options presence: true do
+    validates :knowledge_title, :knowledge_info, :image
+  end
 end

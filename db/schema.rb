82dd68dc32045928eb322fbee74597f43270fce2
sushi1129 <88273491+sushi1129@users.noreply.github.com>
2021-09-14 21:26:16 +0900

    knowledgesテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_14_074220) do
+ActiveRecord::Schema.define(version: 2021_09_14_121558) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -45,6 +45,15 @@ ActiveRecord::Schema.define(version: 2021_09_14_074220) do
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
+  create_table "knowledges", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "knowledge_title", null: false
+    t.text "knowledge_info", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_knowledges_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "email", default: "", null: false
@@ -72,4 +81,5 @@ ActiveRecord::Schema.define(version: 2021_09_14_074220) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "items", "users"
+  add_foreign_key "knowledges", "users"
 end

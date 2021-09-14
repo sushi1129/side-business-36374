43697dda24f25afa56daf438137c59d501dc97c9
sushi1129 <88273491+sushi1129@users.noreply.github.com>
2021-09-14 20:28:14 +0900

    商品情報投稿の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,14 +1,25 @@
 class ItemsController < ApplicationController
-  before_action :move_to_index, except: [:index]
+  before_action :authenticate_user!, only: [:new, :create]
 
   def index
   end
 
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
   private
   
-  def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
-    end
+  def item_params
+    params.require(:item).permit(:item_name, :maker, :image, :plice, :bought_store, :item_info).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,11 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  with_options presence: true do
+    validates :item_name, :maker, :image, :bought_store
+
+    validates :plice, format: { with: /\A[0-9]+\z/, message: 'is invalid. Input half-width characters' },
+                      numericality: true
+  end
 end

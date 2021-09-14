93616049b9f38be23857673dadf9755a79c4f1b8
sushi1129 <88273491+sushi1129@users.noreply.github.com>
2021-09-14 16:36:57 +0900

    itemテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_13_034018) do
+ActiveRecord::Schema.define(version: 2021_09_14_072412) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "item_name", null: false
+    t.string "maker", null: false
+    t.integer "plice", null: false
+    t.string "bought_store", null: false
+    t.text "item_info"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
@@ -37,4 +49,5 @@ ActiveRecord::Schema.define(version: 2021_09_13_034018) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end

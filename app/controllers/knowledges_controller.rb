ec79c169d60a3db7bcf1494600ca111415d508c0
sushi1129 <88273491+sushi1129@users.noreply.github.com>
2021-09-15 11:27:58 +0900

    知識情報投稿の実装

--- app/controllers/knowledges_controller.rb
+++ app/controllers/knowledges_controller.rb
@@ -1,2 +1,25 @@
 class KnowledgesController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
+  def index
+  end
+
+  def new
+    @knowledge = Knowledge.new
+  end
+
+  def create
+    @knowledge = Knowledge.create(knowledge_params)
+    if @knowledge.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def knowledge_params
+    params.require(:knowledge).permit(:knowledge_title, :knowledge_info, :image).merge(user_id: current_user.id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   get 'items/index'
   root to: "items#index"
-  resources :items, only: [:new,:create]
+  resources :items, only: [:new, :create]
+  resources :knowledges, only: [:new, :create]
 end

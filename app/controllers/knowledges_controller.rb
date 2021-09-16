b87cb41f577e31947d73b0d9f1704050d1a04f34
sushi1129 <88273491+sushi1129@users.noreply.github.com>
2021-09-16 20:50:25 +0900

    knowledgeのdestroyアクション実装

--- app/controllers/knowledges_controller.rb
+++ app/controllers/knowledges_controller.rb
@@ -1,6 +1,6 @@
 class KnowledgesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :show, :edit, :update]
-  before_action :set_knowledge, only: [:show, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :create, :show, :edit, :update, :destroy]
+  before_action :set_knowledge, only: [:show, :edit, :update, :destroy]
 
   def index
     @knowledges = Knowledge.order("created_at DESC")
@@ -32,6 +32,11 @@ class KnowledgesController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def destroy
+    @knowledge.destroy
+    redirect_to root_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   get 'knowledges/index'
   root to: "tops#index"
   resources :items
-  resources :knowledges, only: [:index, :new, :create, :show, :edit, :update]
+  resources :knowledges
 end

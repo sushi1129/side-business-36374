999087dfaca32b881233bf7265c4dee4ac8b8df6
sushi1129 <88273491+sushi1129@users.noreply.github.com>
2021-09-16 15:27:12 +0900

    ログイン状態のみ詳細に遷移できるように設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :show]
 
   def index
     @items = Item.order("created_at DESC")

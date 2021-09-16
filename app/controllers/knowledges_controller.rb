d102f93e8abe4ca8c6c884cbde8ab936d5c0401a
sushi1129 <88273491+sushi1129@users.noreply.github.com>
2021-09-16 18:33:50 +0900

    knowledge情報編集完了後の条件分岐実装

--- app/controllers/knowledges_controller.rb
+++ app/controllers/knowledges_controller.rb
@@ -1,5 +1,6 @@
 class KnowledgesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :show]
+  before_action :set_knowledge, only: [:show, :edit, :update]
 
   def index
     @knowledges = Knowledge.order("created_at DESC")
@@ -20,18 +21,26 @@ class KnowledgesController < ApplicationController
   end
 
   def show
-    @knowledge = Knowledge.find(params[:id])
   end
 
   def edit
   end
 
   def update
+    if @knowledge.update(knowledge_params)
+      redirect_to knowledge_path(@knowledge.id)
+    else
+      render :edit
+    end
   end
 
   private
 
   def knowledge_params
     params.require(:knowledge).permit(:knowledge_title, :knowledge_info, :image).merge(user_id: current_user.id)
+  end
+
+  def set_knowledge
+    @knowledge = Knowledge.find(params[:id])
   end
 end

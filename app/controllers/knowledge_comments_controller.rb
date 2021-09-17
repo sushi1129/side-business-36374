40f07857e6a5b058ef548d9a3a4e93954a33d02f
sushi1129 <88273491+sushi1129@users.noreply.github.com>
2021-09-17 17:48:53 +0900

    knowledge_commentの保存とビューの表示

--- app/controllers/knowledge_comments_controller.rb
+++ app/controllers/knowledge_comments_controller.rb
@@ -1,4 +1,18 @@
 class KnowledgeCommentsController < ApplicationController
   def create
+    @knowledge_comment = KnowledgeComment.new(knowledge_comment_params)
+    if @knowledge_comment.save
+      redirect_to knowledge_path(@knowledge_comment.knowledge)
+    else
+      @knowledge = @knowledge_comment.knowledge
+      @knowledge_comment = @knowledge.knowledge_comment
+      render "knowledge/show"
+    end
+  end
+
+  private
+
+  def knowledge_comment_params
+    params.require(:knowledge_comment).permit(:text).merge(user_id: current_user.id, knowledge_id: params[:knowledge_id])
   end
 end

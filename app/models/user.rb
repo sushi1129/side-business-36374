c3be146c490f3b794dcddcf1cbb13fa2a60876bd
sushi1129 <88273491+sushi1129@users.noreply.github.com>
2021-09-17 15:53:17 +0900

    item_commentのアソシエーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :items
+  has_many :item_comments
   has_many :knowledges
   
   extend ActiveHash::Associations::ActiveRecordExtensions

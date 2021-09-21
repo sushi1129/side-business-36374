16405389c8be82e677f944912275e2e641adfcaf
sushi1129 <88273491+sushi1129@users.noreply.github.com>
2021-09-21 14:38:41 +0900

    退会ユーザーの関連投稿も一緒に削除されるように設定

--- app/models/knowledge.rb
+++ app/models/knowledge.rb
@@ -1,7 +1,7 @@
 class Knowledge < ApplicationRecord
   belongs_to :user
-  has_many :knowledge_comments
-  has_one_attached :image
+  has_many :knowledge_comments, dependent: :destroy
+  has_one_attached :image, dependent: :destroy
 
   with_options presence: true do
     validates :knowledge_title, :knowledge_info, :image

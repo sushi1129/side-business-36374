eb84402c1b4bd2f26d0acaf1048678e7555a4583
sushi1129 <88273491+sushi1129@users.noreply.github.com>
2021-10-06 21:04:21 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -15,5 +15,6 @@ Rails.application.routes.draw do
   resources :knowledges do
     resources :knowledge_comments, only: [:create, :destroy]
     resources :knowledge_goods, only: [:create, :destroy]
+    resources :knowledge_bads, only: [:create, :destroy]
   end
 end

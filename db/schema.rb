5e470a07b6fe33b1a433924528625f654881e5da
sushi1129 <88273491+sushi1129@users.noreply.github.com>
2021-10-07 18:07:46 +0900

    item_goodsテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_06_115328) do
+ActiveRecord::Schema.define(version: 2021_10_07_090721) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -41,6 +41,15 @@ ActiveRecord::Schema.define(version: 2021_10_06_115328) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["item_id"], name: "index_item_comments_on_item_id"
     t.index ["user_id"], name: "index_item_comments_on_user_id"
+  end
+
+  create_table "item_goods", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "item_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["item_id"], name: "index_item_goods_on_item_id"
+    t.index ["user_id"], name: "index_item_goods_on_user_id"
   end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -120,6 +129,8 @@ ActiveRecord::Schema.define(version: 2021_10_06_115328) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "item_comments", "items"
   add_foreign_key "item_comments", "users"
+  add_foreign_key "item_goods", "items"
+  add_foreign_key "item_goods", "users"
   add_foreign_key "items", "users"
   add_foreign_key "knowledge_bads", "knowledges"
   add_foreign_key "knowledge_bads", "users"
